c8666697a7843580990b45119ba0efb7c2089749
hilmikt <muhammedhilmi46@gmail.com>
2025-07-08 19:49:14 +0530

    Created event to log every transactions

--- contracts/wallet.sol
+++ contracts/wallet.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.20;
 contract SimpleWallet{
     address public owner;
 
+    event Deposited(address indexed sender, uint amount);
+    event Withdrawn(address indexed to, uint amount);
+
     constructor() {
         owner = msg.sender;
     }
@@ -15,10 +18,24 @@ contract SimpleWallet{
 
     function deposit() public payable {
         require(msg.value > 0, "Send some ETH");
+        emit Deposited(msg.sender, msg.value);
     }
 
     function withdraw(uint amount) public onlyOwner {
         require(address(this).balance >= amount, "Insufficient balance");
         payable(msg.sender).transfer(amount);
+        emit Withdrawn(msg.sender, amount);
+    }
+
+    function getBalance()public view returns(uint){
+        return address(this).balance;
+    }
+
+    receive() external payable {
+        emit Deposited(msg.sender, msg.value);
+    }
+
+    fallback() external payable {
+        emit Deposited(msg.sender, msg.value);
     }
 }
