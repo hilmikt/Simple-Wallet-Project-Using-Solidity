8ab6bedeb0da25376343a6ef8a820d1582f34f17
hilmikt <muhammedhilmi46@gmail.com>
2025-07-08 19:28:32 +0530

    Added functions for deposit and withdraw

--- contracts/wallet.sol
+++ contracts/wallet.sol
@@ -7,5 +7,18 @@ contract SimpleWallet{
     constructor() {
         owner = msg.sender;
     }
-    
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not authorized");
+        _;
+    }
+
+    function deposit() public payable {
+        require(msg.value > 0, "Send some ETH");
+    }
+
+    function withdraw(uint amount) public onlyOwner {
+        require(address(this).balance >= amount, "Insufficient balance");
+        payable(msg.sender).transfer(amount);
+    }
 }
